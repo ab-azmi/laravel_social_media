6f2006e3e84fb4f7fc07856a65850dd14ec664bb
ab-azmi <azmi.kiva@gmail.com>
2024-03-05 03:37:36 +0700

    add username and install slug package

--- app/Http/Requests/ProfileUpdateRequest.php
+++ app/Http/Requests/ProfileUpdateRequest.php
@@ -17,7 +17,17 @@ class ProfileUpdateRequest extends FormRequest
     {
         return [
             'name' => ['required', 'string', 'max:255'],
+            'username' => ['required', 'string', 'max:255', 'regex:/^[A-Za-z0-9\.-]*$/'],
             'email' => ['required', 'string', 'lowercase', 'email', 'max:255', Rule::unique(User::class)->ignore($this->user()->id)],
+        ];
+    }
+
+    //message for no white space
+    // 'regex:/^\S*$/u' => 'No white space allowed',
+    public function messages()
+    {
+        return [
+            'username.regex' => 'Only letters, numbers, dashes and periods are allowed.',
         ];
     }
 }
